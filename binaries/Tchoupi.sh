b1fe7a5670e867e4d4c41a5c8993e03b0fccbc41
Cynthia Serber <cynthia.serber@gmail.com>
2020-05-08 15:32:30 +0200

    [9209] Fix the errors due to the bad deployment

--- binaries/Tchoupi.sh
+++ binaries/Tchoupi.sh
@@ -5,5 +5,7 @@ export LD_LIBRARY_PATH="$BASE_DIR":"$LD_LIBRARY_PATH"
 export QTDIR="$BASE_DIR"
 export TRANSLATION_PATH=../translation
 export VIDEO_TRANSLATION_PATH=../translation/video
+export QT_PLUGIN_PATH="$BASE_DIR"/plugins:"$QT_PLUGIN_PATH"
+export QT_QPA_PLATFORM_PLUGIN_PATH="$BASE_DIR"/plugins/platforms:"$QT_QPA_PLATFORM_PLUGIN_PATH"
 
 $BASE_DIR/Tchoupi &
